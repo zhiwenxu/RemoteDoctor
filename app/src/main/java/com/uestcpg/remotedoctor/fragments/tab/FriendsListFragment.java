3fead6015d6eb920e337178b44961f846a5a5568
POPLX <498130140@qq.com>
2017-06-20 10:10:38 +0800

    xingming:liuxing

--- app/src/main/java/com/uestcpg/remotedoctor/fragments/tab/FriendsListFragment.java
+++ app/src/main/java/com/uestcpg/remotedoctor/fragments/tab/FriendsListFragment.java
@@ -39,6 +39,37 @@ public class FriendsListFragment extends Fragment implements View.OnClickListene
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.fragment_friends,null);
         ButterKnife.inject(this,contentView);
+
+        //new
+
+        //绑定XML中的ListView，作为Item的容器
+        ListView list = (ListView) findViewById(R.id.MyListView);
+
+        //生成动态数组，并且转载数据
+        ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
+        for(int i=0;i<30;i++)
+        {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("ItemTitle", "This is Title.....");
+            map.put("ItemText", "This is text.....");
+            mylist.add(map);
+        }
+        //生成适配器，数组===》ListItem
+        SimpleAdapter mSchedule = new SimpleAdapter(this, //没什么解释
+                mylist,//数据来源
+                R.layout.list_item,//ListItem的XML实现
+
+                //动态数组与ListItem对应的子项
+                new String[] {"ItemTitle", "ItemText"},
+
+                //ListItem的XML文件里面的两个TextView ID
+                new int[] {R.id.ItemTitle,R.id.ItemText});
+        //添加并且显示
+        list.setAdapter(mSchedule);
+
+        //end new
+
+
         init();
         return contentView;
     }
