b056e07b07680de415968264af5db35a9181f865
poplx <poplx9999@qq.com>
2017-06-22 16:14:02 +0800

    姓名：刘行 内容：增加了几行代码 日期：2017/06/22

--- app/src/main/java/com/uestcpg/remotedoctor/DoctorListAdapter.java
+++ app/src/main/java/com/uestcpg/remotedoctor/DoctorListAdapter.java
@@ -4,6 +4,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.uestcpg.remotedoctor.Class.Doctor;
+
+
 /**
  * Created by poplx on 2017/6/21.
  */
@@ -26,6 +29,26 @@ public class DoctorListAdapter extends BaseAdapter{
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        Doctor holder = null;
+        //如果缓存convertView为空，则需要创建View
+        if(convertView == null)
+        {
+            holder = new ViewHolder();
+            //根据自定义的Item布局加载布局
+            convertView = mInflater.inflate(R.layout.list_item, null);
+            holder.img = (ImageView)convertView.findViewById(R.id.img);
+            holder.title = (TextView)convertView.findViewById(R.id.tv);
+            holder.info = (TextView)convertView.findViewById(R.id.info);
+            //将设置好的布局保存到缓存中，并将其设置在Tag里，以便后面方便取出Tag
+            convertView.setTag(holder);
+        }else
+        {
+            holder = (ViewHolder)convertView.getTag();
+        }
+        holder.img.setImageResource((Integer) data.get(position).get("img"));
+        holder.title.setText((String)data.get(position).get("title"));
+        holder.info.setText((String)data.get(position).get("info"));
+
+        return convertView;
     }
 }
--- app/src/main/java/com/uestcpg/remotedoctor/fragments/tab/DoctorListFragment.java
+++ app/src/main/java/com/uestcpg/remotedoctor/fragments/tab/DoctorListFragment.java
@@ -11,8 +11,14 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.uestcpg.remotedoctor.DoctorListAdapter;
 import com.uestcpg.remotedoctor.R;
 import com.uestcpg.remotedoctor.app.AppStatus;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -33,6 +39,8 @@ public class DoctorListFragment extends Fragment implements View.OnClickListener
     @InjectView(R.id.friends_list)
     ListView mFriendsList;
 
+    private List<Map<String, Object>> data;
+
     public static DoctorListFragment getInstance(){
         return new DoctorListFragment();
     }
@@ -43,9 +51,33 @@ public class DoctorListFragment extends Fragment implements View.OnClickListener
         View contentView = inflater.inflate(R.layout.fragment_friends,null);
         ButterKnife.inject(this,contentView);
 
+        data = getData();
+        DoctorListAdapter adapter = new DoctorListAdapter(this);
+        mFriendsList.setAdapter(adapter);
+
         init();
+
+
         return contentView;
     }
+
+    private List<Map<String, Object>> getData()
+    {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map;
+        for(int i=0;i<10;i++)
+        {
+            map = new HashMap<String, Object>();
+            map.put("doctor_photo", R.drawable.doctor);
+            map.put("doctor_name", "李时珍");
+            map.put("doctor_appellation", "主治医师");
+            map.put("doctor_major", "儿科");
+            map.put("advisory", "咨询");
+            list.add(map);
+        }
+        return list;
+    }
+
     private void init(){
         mFriendName.setText(AppStatus.getTagetId());
         mFriendLayout.setOnClickListener(this);
