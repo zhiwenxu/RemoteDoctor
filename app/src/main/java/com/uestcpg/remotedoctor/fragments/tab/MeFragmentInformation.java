4e661209232fbb902d84f2bee3b16d1bc6ba6498
poplx <poplx9999@qq.com>
2017-07-02 17:08:30 +0800

    姓名：刘行 内容：增加了几行代码 日期：2017/06/22

---
+++ app/src/main/java/com/uestcpg/remotedoctor/fragments/tab/MeFragmentInformation.java
@@ -0,0 +1,54 @@
+package com.uestcpg.remotedoctor.fragments.tab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.uestcpg.remotedoctor.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by dmsoft on 2017/6/14.
+ */
+
+public class MeFragmentInformation extends Fragment implements View.OnClickListener{
+
+    public static MeFragmentInformation getInstance(){
+        return new MeFragmentInformation();
+    }
+
+    @InjectView(R.id.change_information_btn)
+    Button Change_Information_btn;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View contentView = inflater.inflate(R.layout.fragment_me_information,null);
+        ButterKnife.inject(this,contentView);
+        init();
+        return contentView;
+    }
+    private void init(){
+
+    }
+
+    private void ChangeInfomation(){
+        Intent intent = new Intent(getActivity(), MeFragmentChange.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == Change_Information_btn){
+            ChangeInfomation();
+        }
+    }
+
+}
